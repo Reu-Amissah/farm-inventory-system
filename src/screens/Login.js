56e96b26c65b1b962663329a0678e7983b236447
Reu-Amissah <reujoyamissah@gmail.com>
2023-05-26 00:06:51 +0000

    edit btn

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -21,8 +21,12 @@ export default function Login() {
             <input type="checkbox"></input> Remember me
             <p>Forgot password</p>
           </div>
-          <button>Sign In</button>
-          <button>Sign in with Google</button>
+          <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm w-full py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            Sign In
+          </button>
+          <button className="focus:outline-none text-green-700 bg-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm w-full py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            Sign in with Google
+          </button>
 
           <p>
             Don't have an account?<p>Sign up for free</p>
